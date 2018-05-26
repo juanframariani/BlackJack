be3c263350bf3c441be6821110ca6a0efd4694f5
Juan Francisco Mariani <36775528+sanfrarow@users.noreply.github.com>
2018-05-26 19:24:42 -0300

    Update main.cpp
    Arreglo del mazo solo de tipo entero.
    Agregado funciones de juego y de repartir cartas.

--- main.cpp
+++ main.cpp
@@ -1,179 +1,166 @@
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
-#include <string>
+#include <string.h>
+#include <stdio.h>
 #define BARAJAR_H
+
 using namespace std;
 
-void barajar(int deck[], int len){
-	int temp = 0;					              //declara una variable temp
-	int randomIndex = 0;			              //declara una variable para el índice a randomizar
-	
-	for(int i=0; i<len; i++){		              //recorre el arreglo
-		randomIndex = rand() % len;	              //le asigna un valor random dentro del rango del largo del arreglo a la variable randomIndex
-		temp = deck[i];				              //le asigna el valor almacenado en el arreglo a la variable temp
-		deck[i] = deck[randomIndex];	          //le asigna el valor de la variable randomIndex al valor i del arreglo
-		deck[randomIndex] = temp;		          //le asigna temp a la posición random del arreglo
-	}
-	
-}
+void barajar(int[],int);
+void nombreJugador(int);
+void juego(int[], int);
+void opcionesJugador();
 
 
 int main (int argc, char *argv[]) {
 	int input;
+	srand(time(NULL));                              	//funciÃ³n random que se conecta con el reloj de la pc para que la secuencia no sea repetitiva
+	int cantJugadores = 0;
+	int contador = 0;
+	int mazo[52];
 	
+	//Menu
 	do{
-
-	cout<<"BLACK JACK\nMENU\nSELECCIONE LA OPCION DESEADA:\n";
-	cout<<"1. UN JUGADOR\n";
-	cout<<"2. MULTIJUGADOR\n";
-	cout<<"3. SALIR\n";
-	cout<<"INGRESE NUMERO DE LA OPCION DESEADA: ";
-	cin>> input;
-	
-	srand(time(NULL));                             //función random que se conecta con el reloj de la pc para que la secuencia no sea repetitiva
-	int deck[52];                                  //declara un arreglo donde se almacenan los números random para evitar que se repitan
-	int position;                                  //declara una variable para acceder a la posición del arreglo
-	string deckUser[10];				//declara un vector para las cartas del usuario
-	string deckCroupier[10];			//declara un vector para las cartas del croupier
-	int posU;					//declara una variable para la posición de las cartas del usuario
-	int posC;					//declara una variable para la posición de las cartas del croupier
-	string mazo[52]={"AP","2P","3P","4P","5P","6P","7P","8P","9P","10P","JP","QP","KP", //declaración del mazo como arreglo de strings
-		"AC","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC",
-		"AT","2T","3T","4T","5T","6T","7T","8T","9T","10T","JT","QT","KT",
-		"AD","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD"};
-
-
-switch(input){
-case 1:
-	for(int i=0; i<52; i++){				 //crea un array de int desde el 0 al 51
-		deck[i]=i;
-	}
-	
-	barajar(deck,52);					//"mezcla" los valores para que no estén ordenados 
-	
-	for(int j=0; j<52; j++){			        //recorre el arreglo completo
-		position=deck[j];				//le asigna el valor del arreglo anterior a position
-	}
-	
-	posU = deck[0];						//asigna la posición del deck a la carta del usuario
-	deckUser[0] = mazo[posU];				//busca en el mazo la posición para mostrarla
-	posC = deck[1];						//asigna la posición del deck a la carta del croupier
-	deckCroupier[0] = mazo[posC];				//busca en el mazo la posición para mostrarla
-	posU = deck[2];
-	deckUser[1] = mazo[posU];
-	posC = deck[1];
-	deckCroupier[1] = mazo[posC];
-	
-	cout << "Tus cartas: " << deckUser[0] << " - " << deckUser[1] << endl; 		//muestra el valor de las cartas repartidas al usuario
-	cout << "Las cartas de la banca: " << deckCroupier[0] << " - XX" << endl; 	//muestra el valor de las cartas repartidas al croupier
-	cout << "\n";
-		if((deckUser[0]=="AP")||(deckUser[0]=="AC")||(deckUser[0]=="AD")||(deckUser[0]=="AT")||(deckUser[1]=="AP")||(deckUser[1]=="AC")||(deckUser[1]=="AD")||(deckUser[1]=="AT")){
-				
-				for(int i; i<2; i++){
-					deckUser[i]=i;
-					cout<< "\n\n";
-					cout<<"CUAL ES EL VALOR DEL AS?\n\n";
-					cout<<" 1 - AS VALE 1\n";
-					cout<<" 2 - AS VALE 11\n\n";
-					int option;
-					cin>>option;
+		
+		cout << "BLACK JACK\nMENU\nSELECCIONE LA OPCION DESEADA:\n";
+		cout << "1. UN JUGADOR\n";
+		cout << "2. MULTIJUGADOR\n";
+		cout << "3. SALIR\n";
+		cout << "INGRESE NUMERO DE LA OPCION DESEADA: ";
+		cin >> input;
+		
+		switch(input){
+		case 1:
+			cantJugadores = 1;
+			nombreJugador(cantJugadores);
+			
+			for (int i=0; i<4; i++){											//creacion del vector de cartas
+				for(int j=0; j<13; j++){
 					
-					switch(option){
-					case 1:
-						deckUser[i]=1;
-						cout<<"EL AS VALE 1";
-						break;
-					case 2:
-						deckUser[i]=11;
-						cout<<"EL AS VALE 11";
-						break;
-					}
-					
+					mazo[contador] = (j+1);
+					contador++;
 				}
-				
 			}
+			
+			barajar(mazo, 52);
+			juego(mazo, cantJugadores);
+			
+		break;
+			
+		case 2:
+			cout << "Ingrese cantidad de jugadores: ";
+			cin >> cantJugadores;
+			nombreJugador(cantJugadores);
+			cout << "Con cuantos mazos desean jugar?: ";
+			
+		break;
+			
+		case 3:
+			return(0);
+		break;
+			
+		default:
+			cout<<"LA OPCION INGRESADA ES INCORRECTA, INTENTE NUEVAMENTE.\n\n";
+		break;
+		}
+		
+	}while(input!=3);
 	
-	break;
-
-case 2:
-	   
-	break;
-case 3:
-	return(0);
-	break;
-default:
-	cout<<"LA OPCION INGRESADA ES INCORRECTA, INTENTE NUEVAMENTE.\n\n";
-	break;
+	cin.get();
 }
 
-}while(input!=3);
 
-cin.get();
 
+//NOMBRE DE LOS JUGADORES
+void nombreJugador(int cantJugadores) {
 	
-}
-
-void nombreJugador() {
-
-    char Nombre[20];
-    int longitud;
-    cout << "Jugador N°1, Ingrese su nombre: ";
-    gets(Nombre);
-    longitud = strlen(Nombre);
-    if (longitud > 20) {
-        cout << "El nombre ingresado es demasiado largo, por favor ingrese uno con menos de 20 caracteres.";
-    } else {
-        cout << "El Nombre ingresado es: " << Nombre;
-    }
-
-
-    return 0;
+	string nombre;
+	int longitud;
+	string nombres[5];
+	
+	for (int i=0; i < cantJugadores; i++) {
+		cout << "Jugador N°" << i+1 << ", Ingrese su nombre: ";
+		cin >> nombre;
+		nombres[i] = nombre;
+	}
 
 }
 
-void turno_croupier() {
-    while (mano[0].conteo_cartas < 17) {
-        pedirCarta(0);
-    }
-    if (mano[0].conteo_cartas >= 17) {
-        paso(0);
-
-    }
 
 
-    return 0;
+
+
+//JUEGO
+void juego(int mazo[], int cantJugadores){
+	int sumaCartas = 0;
+	int sumaCartasCroupier = 0;
+	int cantCartasIniciales = 0;
+	
+	cantCartasIniciales = cantJugadores*2;
+	
+	for (int i=0; i<cantJugadores;i++){
+		cout << "Cartas Jugador " << i+1 << endl;
+		for (int j=0; j<cantCartasIniciales;j++){
+			cout << mazo[j] << endl;
+			sumaCartas += mazo[j];
+		}
+		
+	}
+	int aux = cantCartasIniciales+2;
+	
+	cout << "Cartas Croupier: " << endl;
+	for (int k=aux; k<(aux+2);k++){
+		cout << mazo[k] << endl;
+		sumaCartasCroupier += mazo[k];
+	}
+	
+	opcionesJugador();
 }
 
-
- void opcionesJugador(){
+//OPCIONES DE JUEGO
+void opcionesJugador(){
 	
 	int opcion;
-			/*cout << "Tus cartas: " << deckUser[0] << " - " << deckUser[1] << endl;*/
-	cout<<"La suma de tus cartas es: "<< "¿Qué deseas hacer?\n\n";
+
+	cout<< "¿Qué deseas hacer?\n\n";
+	
 	do{
-	cout<<"1) Pedir una carta más\n";
-	cout<<"2) Plantarse\n";
-	cout<<"3) Separar las cartas\n";
-	cin>>opcion;
-	
-	switch(opcion){
-	case 1:
-	
-	break;
-	
-	case 2:
-	
-	break;
-	
-	case 3:
-	
-	break;
-	
-	default:
-	cout<<"LA OPCION INGRESADA ES INCORRECTA, INTENTE NUEVAMENTE.\n\n";
-	break;
-	
-	}
+		cout<<"1) Pedir una carta más\n";
+		cout<<"2) Plantarse\n";
+		cout<<"3) Separar las cartas\n";
+		cin>>opcion;
+		
+		switch(opcion){
+		case 1:
+			
+		break;
+			
+		case 2:
+			
+		break;
+			
+		case 3:
+			
+		break;
+			
+		default:
+			cout<<"LA OPCION INGRESADA ES INCORRECTA, INTENTE NUEVAMENTE.\n\n";
+		break;
+		}
 	} while(opcion!=4);
+}
+
+
+//FUNCION PARA BARAJAR LAS CARTAS
+void barajar(int mazo[], int len){
+	int temp;					              //declara una variable temp
+	int randomIndex = 0;			              //declara una variable para el Ã­ndice a randomizar
+	
+	for(int i=0; i<len; i++){		              //recorre el arreglo
+		randomIndex = rand() % len;	              //le asigna un valor random dentro del rango del largo del arreglo a la variable randomIndex
+		temp = mazo[i];				              //le asigna el valor almacenado en el arreglo a la variable temp
+		mazo[i] = mazo[randomIndex];	          //le asigna el valor de la variable randomIndex al valor i del arreglo
+		mazo[randomIndex] = temp;		          //le asigna temp a la posiciÃ³n random del arreglo
 	}
+}
