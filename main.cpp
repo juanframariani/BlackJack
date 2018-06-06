80d6719c5f6bb909f41ed25b1939246315573dae
Juan Francisco Mariani <36775528+sanfrarow@users.noreply.github.com>
2018-06-06 14:59:33 -0300

    Update main.cpp
    se borro el menu y se volvieron a codificar algunas funciones del programa

--- main.cpp
+++ main.cpp
@@ -1,166 +1,91 @@
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <string.h>
 #include <stdio.h>
-#define BARAJAR_H
 
 using namespace std;
 
-void barajar(int[],int);
-void nombreJugador(int);
-void juego(int[], int);
-void opcionesJugador();
-
-
 int main (int argc, char *argv[]) {
-	int input;
-	srand(time(NULL));                              	//funciÃ³n random que se conecta con el reloj de la pc para que la secuencia no sea repetitiva
-	int cantJugadores = 0;
-	int contador = 0;
+	
 	int mazo[52];
+	int contador = 0;
+	int temp;					              
+	int randomIndex = 0;	
+	int indice = 0;
+	int sumaJugador = 0;
+	int sumaCroupier = 0;
+	int opcionDeJuego;
 	
-	//Menu
-	do{
-		
-		cout << "BLACK JACK\nMENU\nSELECCIONE LA OPCION DESEADA:\n";
-		cout << "1. UN JUGADOR\n";
-		cout << "2. MULTIJUGADOR\n";
-		cout << "3. SALIR\n";
-		cout << "INGRESE NUMERO DE LA OPCION DESEADA: ";
-		cin >> input;
-		
-		switch(input){
-		case 1:
-			cantJugadores = 1;
-			nombreJugador(cantJugadores);
-			
-			for (int i=0; i<4; i++){											//creacion del vector de cartas
-				for(int j=0; j<13; j++){
-					
-					mazo[contador] = (j+1);
-					contador++;
-				}
-			}
-			
-			barajar(mazo, 52);
-			juego(mazo, cantJugadores);
-			
-		break;
-			
-		case 2:
-			cout << "Ingrese cantidad de jugadores: ";
-			cin >> cantJugadores;
-			nombreJugador(cantJugadores);
-			cout << "Con cuantos mazos desean jugar?: ";
-			
-		break;
-			
-		case 3:
-			return(0);
-		break;
-			
-		default:
-			cout<<"LA OPCION INGRESADA ES INCORRECTA, INTENTE NUEVAMENTE.\n\n";
-		break;
+	//Creacion del Mazo de 52 Cartas, sin pinta
+	for (int i=0; i<4; i++){											
+		for(int j=0; j<13; j++){
+			mazo[contador] = (j+1);
+			contador++;
 		}
-		
-	}while(input!=3);
-	
-	cin.get();
-}
-
-
-
-//NOMBRE DE LOS JUGADORES
-void nombreJugador(int cantJugadores) {
-	
-	string nombre;
-	int longitud;
-	string nombres[5];
-	
-	for (int i=0; i < cantJugadores; i++) {
-		cout << "Jugador N°" << i+1 << ", Ingrese su nombre: ";
-		cin >> nombre;
-		nombres[i] = nombre;
 	}
-
-}
-
-
-
-
-
-//JUEGO
-void juego(int mazo[], int cantJugadores){
-	int sumaCartas = 0;
-	int sumaCartasCroupier = 0;
-	int cantCartasIniciales = 0;
 	
-	cantCartasIniciales = cantJugadores*2;
+	//Random del Mazo
+	srand(time(NULL));
+	for(int i=0; i<52; i++){		              
+		randomIndex = rand() % 52;	              
+		temp = mazo[i];				
+		mazo[i] = mazo[randomIndex];
+		mazo[randomIndex] = temp;	
+	}
 	
-	for (int i=0; i<cantJugadores;i++){
-		cout << "Cartas Jugador " << i+1 << endl;
-		for (int j=0; j<cantCartasIniciales;j++){
-			cout << mazo[j] << endl;
-			sumaCartas += mazo[j];
+	//Repartir las cartas y suma los valores de las mismas
+	for (int i=0; i<2; i++){
+		for (int j=0; j<1; j++){
+			cout << "Jugador: " << mazo[indice] << endl;
+			sumaJugador += mazo[indice];
+			indice++;
 		}
-		
-	}
-	int aux = cantCartasIniciales+2;
-	
-	cout << "Cartas Croupier: " << endl;
-	for (int k=aux; k<(aux+2);k++){
-		cout << mazo[k] << endl;
-		sumaCartasCroupier += mazo[k];
+		cout << "Croupier: " << mazo[indice] << endl;
+		sumaCroupier += mazo[indice];
+		indice++;
 	}
 	
-	opcionesJugador();
-}
-
-//OPCIONES DE JUEGO
-void opcionesJugador(){
+	//Muestra los totales de la suma de las cartas.
+	cout << "Total Jugador: " << sumaJugador << endl;
 	
-	int opcion;
-
-	cout<< "¿Qué deseas hacer?\n\n";
+	//deberia mostrar el total, despues de que que juegan todos los jugadores
+	cout << "Total Croupier: " << sumaCroupier << endl;
 	
-	do{
-		cout<<"1) Pedir una carta más\n";
-		cout<<"2) Plantarse\n";
-		cout<<"3) Separar las cartas\n";
-		cin>>opcion;
-		
-		switch(opcion){
-		case 1:
-			
-		break;
-			
-		case 2:
-			
-		break;
-			
-		case 3:
-			
-		break;
-			
-		default:
-			cout<<"LA OPCION INGRESADA ES INCORRECTA, INTENTE NUEVAMENTE.\n\n";
-		break;
+	//Lazo para pedir o plantarse
+	while(sumaJugador<21){
+		cout << "Que desea hacer?" << endl;
+		cout << "1- Pedir  2- Plantarse" << endl;
+		cin >> opcionDeJuego;
+		if (opcionDeJuego == 1){
+			indice++;
+			cout << "Carta: " << mazo[indice] << endl;
+			sumaJugador += mazo[indice];
+			cout << "Total Jugador: " << sumaJugador << endl;
 		}
-	} while(opcion!=4);
-}
-
-
-//FUNCION PARA BARAJAR LAS CARTAS
-void barajar(int mazo[], int len){
-	int temp;					              //declara una variable temp
-	int randomIndex = 0;			              //declara una variable para el Ã­ndice a randomizar
-	
-	for(int i=0; i<len; i++){		              //recorre el arreglo
-		randomIndex = rand() % len;	              //le asigna un valor random dentro del rango del largo del arreglo a la variable randomIndex
-		temp = mazo[i];				              //le asigna el valor almacenado en el arreglo a la variable temp
-		mazo[i] = mazo[randomIndex];	          //le asigna el valor de la variable randomIndex al valor i del arreglo
-		mazo[randomIndex] = temp;		          //le asigna temp a la posiciÃ³n random del arreglo
+		else
+			break;
 	}
+	
+	//Juego del Croupier
+	while(sumaCroupier<17){
+		cout << "El Croupier tiene menos de 17, pedira otra carta" << endl;
+		indice++;
+		cout << "Carta: " << mazo[indice] << endl;
+		sumaCroupier += mazo[indice];
+		cout << "Total Croupier: " << sumaCroupier << endl;
+	}
+	
+	//Anlasis de puntos para determinar quien gana.
+	if ((sumaCroupier < sumaJugador) && (sumaJugador<22)){
+		cout << "El jugador Gana" << endl;
+	}else if(sumaJugador == 21){
+		cout << "JUGADOR TIENE BLACKJACK" << endl;
+	}else if ((sumaCroupier > sumaJugador) && (sumaCroupier<22)){
+		cout << "El croupier gana" << endl;
+	}else if (sumaCroupier == sumaJugador){
+		cout << "El jugador y el croupier empatan" << endl;
+	}
+	
+return 0;
 }
